6ee099c240c1cdeef6d289501454baef483980ed
Parker Williamson <parker.williamson@gmail.com>
2020-05-04 20:50:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Pretty Bird!")
+def parrot(keyword)
   puts "Squawk!"
-  return "Squawk!"
+  return "Pretty Bird!"
   "Squawk!"
 end
